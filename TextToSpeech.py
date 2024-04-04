549424943c087d48112cf3c2c1a5b864da92ae1b
Jack Weatherbe <97712079+jweaths@users.noreply.github.com>
2024-04-04 16:51:18 -0700

    please god

---
+++ TextToSpeech.py
@@ -0,0 +1,48 @@
+import qi
+import sys
+import time
+import cv2
+import numpy as np
+
+class Authenticator:
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    # This method is expected by libqi and must return a dictionary containing
+    # login information with the keys 'user' and 'token'.
+    def initialAuthData(self):
+        return {'user': self.username, 'token': self.password}
+
+
+class AuthenticatorFactory:
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    # This method is expected by libqi and must return an object with at least
+    # the `initialAuthData` method.
+    def newAuthenticator(self):
+        return Authenticator(self.username, self.password)
+
+# Connect to the robot fails at app.start() => RuntimeError: disconnected
+app = qi.Application(sys.argv, url="tcps://10.0.0.17:9503")
+logins = ("nao", "nao")
+factory = AuthenticatorFactory(*logins)
+app.session.setClientAuthenticatorFactory(factory)
+app.start()
+print("started")
+
+tts = app.session.service("ALTextToSpeech")
+# tts.say("Hello, how are you")
+
+order = int(sys.argv[1])
+# print(sys.argv)
+
+if order == 0:
+    tts.say("Hello")
+
+elif order == 1:
+    tts.say("Hello")
